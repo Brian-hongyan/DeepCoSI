159e8f5cf0633b914b7caa2f9e820736022740b6
Brian-hongyan <63146755+Brian-hongyan@users.noreply.github.com>
2021-11-21 11:10:41 +0800

    Add files via upload

---
+++ build/example_chimera_py/4hqr_A_70.py
@@ -0,0 +1,5 @@
+from chimera import runCommand 
+runCommand('open 0 ./build/4hqr_process.pdb') 
+runCommand('select :70.A z<15') 
+runCommand('write format pdb selected 0 ./build/example_pocket/4hqr_A_70_pocket.pdb') 
+runCommand('close 0')
